2e3be9b93584fc6e9ffa67c3b593ba4ced04283e
Matthew Folley <Matthew.Folley@os.uk>
2019-05-28 12:59:29 +0100

    Passing tests

--- tennis_game.py
+++ tennis_game.py
@@ -1,3 +1,29 @@
 class TennisGame:
+    score_map = {3: 'forty', 2: 'thirty', 1: 'fifteen', 0: 'love'}
+
+    def __init__(self):
+        self.player1 = Player()
+        self.player2 = Player()
+
     def score(self):
-        return "fifteen-love"
+        if self.player1.points >= 5 and self.player1.points - self.player2.points >= 2:
+            return "player1 wins"
+        if self.player2.points >= 5 and self.player2.points - self.player1.points >= 2:
+            return "player2 wins"
+        if self.player1.points >= 4 and self.player1.points > self.player2.points:
+            return "advantage player1"
+        if self.player2.points >= 4 and self.player2.points > self.player1.points:
+            return "advantage player2"
+        if self.player1.points == self.player2.points:
+            if self.player1.points >= 3:
+                return "deuce"
+            return TennisGame.score_map[self.player1.points] + "-" + "all"
+        return TennisGame.score_map[self.player1.points] + "-" + TennisGame.score_map[self.player2.points]
+
+
+class Player:
+    def __init__(self):
+        self.points = 0
+
+    def wins_point(self):
+        self.points += 1
--- tennis_game_tests.py
+++ tennis_game_tests.py
@@ -3,10 +3,124 @@ from tennis_game import TennisGame
 
 
 class TestTennisGame(unittest.TestCase):
-    def test_prints_fifteen_love_when_score_1_0(self):
-        tennis_game = TennisGame()
-        self.assertEqual(tennis_game.score(), "fifteen-love")
+    def player_wins_points(self, player, points):
+        for i in range(points):
+            player.wins_point()
 
+    def test_returns_fifteen_love_when_score_1_0(self):
+        # arrange
+        tennis_game = TennisGame()
+        # act
+        tennis_game.player1.wins_point()
+        # assert
+        self.assertEqual("fifteen-love", tennis_game.score())
+
+    def test_returns_fifteen_all_when_score_1_1(self):
+        # arrange
+        tennis_game = TennisGame()
+        # act
+        tennis_game.player1.wins_point()
+        tennis_game.player2.wins_point()
+        # assert
+        self.assertEqual("fifteen-all", tennis_game.score())
+
+    def test_returns_love_fifteen_when_score_0_1(self):
+        # arrange
+        tennis_game = TennisGame()
+        # act
+        tennis_game.player2.wins_point()
+        # assert
+        self.assertEqual("love-fifteen", tennis_game.score())
+
+    def test_returns_thirty_fifteen_when_score_2_1(self):
+        # arrange
+        tennis_game = TennisGame()
+        # act
+        self.player_wins_points(tennis_game.player1, 2)
+        tennis_game.player2.wins_point()
+        # assert
+        self.assertEqual("thirty-fifteen", tennis_game.score())
+
+    def test_returns_thirty_forty_when_score_2_3(self):
+        # arrange
+        tennis_game = TennisGame()
+        # act
+        self.player_wins_points(tennis_game.player1, 2)
+        self.player_wins_points(tennis_game.player2, 3)
+        # assert
+        self.assertEqual("thirty-forty", tennis_game.score())
+
+    def test_returns_deuce_when_score_3_3(self):
+        # arrange
+        tennis_game = TennisGame()
+        # act
+        self.player_wins_points(tennis_game.player1, 3)
+        self.player_wins_points(tennis_game.player2, 3)
+        # assert
+        self.assertEqual("deuce", tennis_game.score())
+
+    def test_returns_advantage_player1_when_score_4_3(self):
+        # arrange
+        tennis_game = TennisGame()
+        # act
+        self.player_wins_points(tennis_game.player1, 4)
+        self.player_wins_points(tennis_game.player2, 3)
+        # assert
+        self.assertEqual("advantage player1", tennis_game.score())
+
+    def test_returns_advantage_player2_when_score_3_4(self):
+        # arrange
+        tennis_game = TennisGame()
+        # act
+        self.player_wins_points(tennis_game.player1, 3)
+        self.player_wins_points(tennis_game.player2, 4)
+        # assert
+        self.assertEqual("advantage player2", tennis_game.score())
+
+    def test_returns_advantage_player1_when_score_5_4(self):
+        # arrange
+        tennis_game = TennisGame()
+        # act
+        self.player_wins_points(tennis_game.player1, 5)
+        self.player_wins_points(tennis_game.player2, 4)
+        # assert
+        self.assertEqual("advantage player1", tennis_game.score())
+
+    def test_returns_advantage_player2_when_score_5_6(self):
+        # arrange
+        tennis_game = TennisGame()
+        # act
+        self.player_wins_points(tennis_game.player1, 5)
+        self.player_wins_points(tennis_game.player2, 6)
+        # assert
+        self.assertEqual("advantage player2", tennis_game.score())
+
+    def test_returns_player1_wins_when_score_5_3(self):
+        # arrange
+        tennis_game = TennisGame()
+        # act
+        self.player_wins_points(tennis_game.player1, 5)
+        self.player_wins_points(tennis_game.player2, 3)
+        # assert
+        self.assertEqual("player1 wins", tennis_game.score())
+
+    def test_returns_player2_wins_when_score_9_11(self):
+        # arrange
+        tennis_game = TennisGame()
+        # act
+        self.player_wins_points(tennis_game.player1, 9)
+        self.player_wins_points(tennis_game.player2, 11)
+        # assert
+        self.assertEqual("player2 wins", tennis_game.score())
+
+    def test_returns_deuce_score_16_16(self):
+        # arrange
+        tennis_game = TennisGame()
+        # act
+        self.player_wins_points(tennis_game.player1, 16)
+        self.player_wins_points(tennis_game.player2, 16)
+        # assert
+        self.assertEqual("deuce", tennis_game.score())
 
 if __name__ == '__main__':
     unittest.main()
